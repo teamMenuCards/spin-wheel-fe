cafbbd0af461c6d549013a46e41a38d11794a5f3
teamMenuCards <getmenucards@gmail.com>
2025-01-03 16:38:33 +0530

    fix: fixed type issue

--- src/context/restoDetailsProvider.tsx
+++ src/context/restoDetailsProvider.tsx
@@ -3,17 +3,30 @@ import React, { useContext, createContext, useState } from "react"
 const useRestaurantContext = () => useContext(WhatsappMssgContext)
 
 const WhatsappMssgContext = createContext({
-	restaurantDetails: { display_name: "" },
+	restaurantDetails: { display_name: "", detail: { phone_no: "" } },
 	setRestaurantDetails: ({}) => undefined
 })
+
+interface IValues {
+	restaurantDetails: {
+		display_name: string
+		detail: {
+			phone_no: string
+		}
+	}
+	setRestaurantDetails: (value) => any
+}
 
 // Create a provider component to wrap your application
 export const RestaurantDetailsProvider = ({ children }) => {
 	const [restaurantDetails, setRestaurantDetails] = useState({
-		display_name: ""
+		display_name: "",
+		detail: {
+			phone_no: ""
+		}
 	})
 
-	const values = {
+	const values: IValues = {
 		restaurantDetails,
 		setRestaurantDetails
 	}

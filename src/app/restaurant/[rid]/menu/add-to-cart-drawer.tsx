89dacbdba5b38691d31f3cc20a6f4a1542868bcc
teamMenuCards <getmenucards@gmail.com>
2025-01-03 16:46:55 +0530

    fix: removed file

--- src/app/restaurant/[rid]/menu/add-to-cart-drawer.tsx
+++ src/app/restaurant/[rid]/menu/add-to-cart-drawer.tsx
@@ -1,37 +1,11 @@
 "use client"
 import React, { useEffect, useState } from "react"
-import {
-	Drawer,
-	Radio,
-	Grid,
-	Button,
-	Box,
-	Typography,
-	styled,
-	List,
-	ListItem,
-	FormControlLabel,
-	IconButton
-} from "@mui/material"
+import { Drawer, Button, Box, Typography, IconButton } from "@mui/material"
 import Image from "next/image"
 import CloseIcon from "@mui/icons-material/Close"
-import NutrientComponent from "./nutrients"
 import { Ic_nonveg, Ic_veg } from "@mcc/icons"
 import { isSafeArray } from "@mcc/helpers/utils"
 import { useCart } from "@mcc/context"
-
-const StyledContainer = styled(Box)(() => ({
-	display: "flex",
-	justifyContent: "space-between",
-	alignItems: "center",
-	width: "100%"
-}))
-
-const Wrapper = styled(Box)(() => ({
-	display: "flex",
-	justifyContent: "space-between",
-	alignItems: "center"
-}))
 
 function AddToCartDrawer({ onClickAdd }) {
 	const {
@@ -66,28 +40,6 @@ function AddToCartDrawer({ onClickAdd }) {
 	}, [products])
 
 	const [variant, setVariant] = useState()
-	const [selectedOption, setSelectedOption] = useState()
-
-	const handleChange = (event, option, index, variant = {}) => {
-		setSelectedOption(event.target.value)
-		if (Object.keys(variant).length) {
-			setVariant({
-				...variant,
-				id: `${index}_${variant.name}`,
-				selected: {
-					...option
-				}
-			})
-		} else {
-			setVariant({
-				...option,
-				id: `${index}_${option.name}`,
-				selected: {
-					...option
-				}
-			})
-		}
-	}
 
 	useEffect(() => {
 		const handlePopState = () => {

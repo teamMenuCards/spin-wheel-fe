1e65cafa96f5c25d60cadad3025ea7842eff82a6
teamMenuCards <getmenucards@gmail.com>
2025-01-29 17:05:25 +0530

    fix: made changes for adding star ratings

--- src/app/restaurant/[rname]/menu/components/menu-item.tsx
+++ src/app/restaurant/[rname]/menu/components/menu-item.tsx
@@ -7,6 +7,9 @@ function MenuItem({
 }: {
 	product: ProductType & { variants: ProductVariantType[] }
 }) {
+	const rating: number = Number(product.variants?.[0]?.average_rating) || 4
+	const starsArray: number[] = Array.from({ length: rating })
+
 	if (!product) {
 		return null
 	}
@@ -78,6 +81,25 @@ function MenuItem({
 								₹{product.variants[0].price}
 							</p>
 						)}
+
+						<div className="flex">
+							{product.variants?.[0]?.average_rating &&
+								starsArray.map((_, index) => {
+									return (
+										<div key={index}>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="#ffb101"
+												viewBox="0 0 24 24"
+												width="18px"
+												height="18px"
+											>
+												<path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+											</svg>
+										</div>
+									)
+								})}
+						</div>
 					</div>
 				</div>
 			</div>

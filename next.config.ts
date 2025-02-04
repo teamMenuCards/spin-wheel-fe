5ee8891932100c7fcec2cafd86431a6c30c47c37
teamMenuCards <getmenucards@gmail.com>
2025-02-04 21:59:24 +0530

    fix: added swiggy domain in next config

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
 			{
 				protocol: "https",
 				hostname: "b.zmtcdn.com"
+			},
+			{
+				protocol: "https",
+				hostname: "media-assets.swiggy.com"
 			}
 		]
 	}

cd20698e57a5cd3022c28535867d8d27240422a3
teamMenuCards <getmenucards@gmail.com>
2025-03-10 09:30:22 +0530

    added changes for available timings (#24)
    * fix: updated changes for ferhcing section as per time
    
    * changes for handling overnight cases
    
    * added safety chks

--- src/app/restaurant/[rname]/menu/page.tsx
+++ src/app/restaurant/[rname]/menu/page.tsx
@@ -8,6 +8,7 @@ import Accordion from "@/shared/Accordian"
 import FloatingMenu from "./components/floating-menu"
 import NavBar from "./components/NavBar"
 import { useGetRestaurantDetailByNameQuery } from "@/services/restaurant/get-restaurant-detail"
+import { ProductCategoryType } from "@/types"
 
 export default function MenuPage() {
 	const { rname } = useParams<{ rname: string }>()
@@ -18,8 +19,43 @@ export default function MenuPage() {
 		console.log(category)
 	}
 
+	const isCurrentTimeWithinRange = (
+		availableFrom: ProductCategoryType["available_from"],
+		availableTo: ProductCategoryType["available_to"]
+	) => {
+		const now = new Date()
+		const currentTime = now.getHours() * 60 + now.getMinutes() // Local time
+
+		const [fromHours, fromMinutes] = availableFrom
+			.split(":")
+			.slice(0, 2)
+			.map(Number)
+		const [toHours, toMinutes] = availableTo.split(":").slice(0, 2).map(Number)
+
+		const fromTime = fromHours * 60 + fromMinutes
+		const toTime = toHours * 60 + toMinutes
+
+		// Handle overnight case
+		const isWithinRange =
+			fromTime <= toTime
+				? currentTime >= fromTime && currentTime <= toTime
+				: currentTime >= fromTime || currentTime <= toTime
+
+		return isWithinRange
+	}
+
+	const validCategories = menudata?.categories.filter((item) => {
+		const isTimingsAvailable = item?.available_from && item?.available_to
+
+		if (isTimingsAvailable) {
+			return isCurrentTimeWithinRange(item?.available_from, item?.available_to)
+		}
+
+		return menudata?.categories
+	})
+
 	const sortedCategories =
-		menudata?.categories
+		validCategories
 			?.slice()
 			.sort((a, b) => a.display_order - b.display_order) ?? []
 

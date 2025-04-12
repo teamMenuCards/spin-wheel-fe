42d2a9af9c60508a77a4343fa272124faad4b268
teamMenuCards <getmenucards@gmail.com>
2025-04-13 04:16:51 +0530

    type fixe

--- src/shared/ImageOverlay.tsx
+++ src/shared/ImageOverlay.tsx
@@ -4,7 +4,7 @@ function ImageOverlay({
 	imageUrl,
 	onClose
 }: {
-	imageUrl: string
+	imageUrl: string | null
 	onClose: () => void
 }) {
 	return (
@@ -13,17 +13,19 @@ function ImageOverlay({
 			className="fixed z-50 inset-0 flex justify-center items-center"
 			onClick={onClose}
 		>
-			<div className="p-4" onClick={onClose}>
-				<Image
-					unoptimized
-					className="z-200 rounded-xl"
-					src={imageUrl}
-					alt="Full View"
-					width={300}
-					height={300}
-					priority
-				/>
-			</div>
+			{imageUrl ? (
+				<div className="p-4" onClick={onClose}>
+					<Image
+						unoptimized
+						className="z-200 rounded-xl"
+						src={imageUrl}
+						alt="Full View"
+						width={300}
+						height={300}
+						priority
+					/>
+				</div>
+			) : null}
 		</div>
 	)
 }

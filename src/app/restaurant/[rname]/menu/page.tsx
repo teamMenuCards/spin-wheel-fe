c60841162eccef04654a0e3fd59ca3503b6e9735
teamMenuCards <getmenucards@gmail.com>
2025-04-15 03:01:44 +0530

    reverted changes

--- src/app/restaurant/[rname]/menu/page.tsx
+++ src/app/restaurant/[rname]/menu/page.tsx
@@ -8,7 +8,7 @@ import Accordion from "@/shared/Accordian"
 import FloatingMenu from "./components/floating-menu"
 import NavBar from "./components/NavBar"
 import { useGetRestaurantDetailByNameQuery } from "@/services/restaurant/get-restaurant-detail"
-// import { ProductCategoryType } from "@/types"
+import { ProductCategoryType } from "@/types"
 import { isSafeArray } from "@/utils/isSafeArray"
 import Loading from "./loading"
 
@@ -25,37 +25,37 @@ export default function MenuPage() {
 		console.log(category)
 	}
 
-	// const isCurrentTimeWithinRange = (
-	// 	availableFrom: ProductCategoryType["available_from"],
-	// 	availableTo: ProductCategoryType["available_to"]
-	// ) => {
-	// 	const now = new Date()
-	// 	const currentTime = now.getHours() * 60 + now.getMinutes() // Local time
+	const isCurrentTimeWithinRange = (
+		availableFrom: ProductCategoryType["available_from"],
+		availableTo: ProductCategoryType["available_to"]
+	) => {
+		const now = new Date()
+		const currentTime = now.getHours() * 60 + now.getMinutes() // Local time
 
-	// 	const [fromHours, fromMinutes] = availableFrom
-	// 		.split(":")
-	// 		.slice(0, 2)
-	// 		.map(Number)
-	// 	const [toHours, toMinutes] = availableTo.split(":").slice(0, 2).map(Number)
+		const [fromHours, fromMinutes] = availableFrom
+			.split(":")
+			.slice(0, 2)
+			.map(Number)
+		const [toHours, toMinutes] = availableTo.split(":").slice(0, 2).map(Number)
 
-	// 	const fromTime = fromHours * 60 + fromMinutes
-	// 	const toTime = toHours * 60 + toMinutes
+		const fromTime = fromHours * 60 + fromMinutes
+		const toTime = toHours * 60 + toMinutes
 
-	// 	// Handle overnight case
-	// 	const isWithinRange =
-	// 		fromTime <= toTime
-	// 			? currentTime >= fromTime && currentTime <= toTime
-	// 			: currentTime >= fromTime || currentTime <= toTime
+		// Handle overnight case
+		const isWithinRange =
+			fromTime <= toTime
+				? currentTime >= fromTime && currentTime <= toTime
+				: currentTime >= fromTime || currentTime <= toTime
 
-	// 	return isWithinRange
-	// }
+		return isWithinRange
+	}
 
-	const validCategories = menudata?.categories.filter(() => {
-		// const isTimingsAvailable = item?.available_from && item?.available_to
+	const validCategories = menudata?.categories.filter((item) => {
+		const isTimingsAvailable = item?.available_from && item?.available_to
 
-		// if (isTimingsAvailable) {
-		// 	return isCurrentTimeWithinRange(item?.available_from, item?.available_to)
-		// }
+		if (isTimingsAvailable) {
+			return isCurrentTimeWithinRange(item?.available_from, item?.available_to)
+		}
 
 		return menudata?.categories
 	})

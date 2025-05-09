c5147c8bf7582d87c9d3c60e0810f4ae354f4c8c
teamMenuCards <getmenucards@gmail.com>
2025-05-09 10:46:50 +0530

    style

--- src/app/restaurant/[rname]/shared/InfoButton.tsx
+++ src/app/restaurant/[rname]/shared/InfoButton.tsx
@@ -51,15 +51,16 @@ const InfoButton: React.FC<Props> = (props) => {
 							className="mr-4 w-6 h-6 drop-shadow-sm rounded"
 						/>
 					) : null}
+
 					{/* Button Text */}
-					<div className="font-semibold text-black text-sm text-center flex justify-center items-center">
-						<div className="font-semibold text-sm text-left flex justify-center items-center">
-							{props.value}
-						</div>
+
+					<div className="font-semibold text-sm text-black text-left flex justify-center items-center">
+						{props.value}
 					</div>
-					<div className="font-semibold text-white rounded-bl-lg rounded-br-lg bg-lime-500 text-xs text-center flex justify-center items-center">
-						For first 20 customers only
-					</div>
+				</div>
+
+				<div className="font-semibold text-white rounded-bl-lg rounded-br-lg bg-lime-500 text-xs text-center flex justify-center items-center">
+					For first 20 customers only
 				</div>
 			</div>
 		</NextLink>
@@ -94,7 +95,6 @@ const InfoButton: React.FC<Props> = (props) => {
 			</div>
 		</NextLink>
 	)
-
 	return <>{props.premium ? premiumBtn() : defaultBtn()}</>
 }
 

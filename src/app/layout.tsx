7ed50009306369e8edd99d88e5ef3e97c0b9cb55
teamMenuCards <getmenucards@gmail.com>
2025-05-11 12:02:10 +0530

    commnented Gtm script tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import "./globals.css"
 import Script from "next/script"
-import { GoogleTagManager } from "@next/third-parties/google"
+// import { GoogleTagManager } from "@next/third-parties/google"
 import { Geist, Geist_Mono, Montserrat } from "next/font/google"
 
 import { RootProvider } from "./providers/RootProvider"
@@ -38,23 +38,9 @@ export default function RootLayout({
 		<html lang="en">
 			<head>
 				{/* Google Tag Manager Script */}
-				<GoogleTagManager gtmId="GTM-N6WPVHXW" />
+				{/* <GoogleTagManager gtmId="GTM-N6WPVHXW" /> */}
 
 				{/* Microsoft Clarity Script */}
-
-				{/* <Script
-					id="gtm-script"
-					strategy="afterInteractive"
-					dangerouslySetInnerHTML={{
-						__html: `
-								(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-								new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-								j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-								'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-								})(window,document,'script','dataLayer','N6WPVHXW');
-							`
-					}}
-				/> */}
 				<Script
 					id="clarity-script"
 					strategy="afterInteractive"

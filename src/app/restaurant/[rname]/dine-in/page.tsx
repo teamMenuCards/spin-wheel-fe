9fd8bcf5ff8727e54e4c70d950b2a767a6652290
teamMenuCards <getmenucards@gmail.com>
2025-07-01 22:51:50 +0530

    changes for review feature (#56)

--- src/app/restaurant/[rname]/dine-in/page.tsx
+++ src/app/restaurant/[rname]/dine-in/page.tsx
@@ -27,7 +27,9 @@ export default async function Page({
 
 	return (
 		<div>
-			<DineinLandingPage rname={rname} restaurantInfo={restaurantDetails} />
+			{restaurantDetails && (
+				<DineinLandingPage rname={rname} restaurantInfo={restaurantDetails} />
+			)}
 			{/* changes for ScrollButton */}
 			<ScrollButton />
 			<Footer />
